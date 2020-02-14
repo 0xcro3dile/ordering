f6fb4841d93ea61be95f422c9d949a456a603f41
ahmed abbas <ahmed.abbas@linuxmail.org>
2020-02-14 11:54:02 -0500

    initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->boolean('verified_email')->default(false);
             $table->boolean('verified_mobile')->default(false);
             $table->unsignedBigInteger('billing_address');
-            $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedBigInteger('shipping_address');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
